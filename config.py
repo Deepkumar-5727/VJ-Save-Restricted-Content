8093cbc1dc35975fe54919ca6cf245b33febb00e
Deepkumar-5727 <deep_kumar@myyahoo.com>
2024-12-12 10:12:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://motorbsoothing03:FXj6wDr6XW4BXTwx@cluster0.2kg1s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodster0.2kg1s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
