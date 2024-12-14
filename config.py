f7b517d849d19d2d3d4bf1b6446f62b651f93e13
Deepkumar-5727 <deep_kumar@myyahoo.com>
2024-12-14 12:40:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", ":-)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", jsj":-)
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "hejs"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", ")
+API_HASH = os.environ.get("API_HASH", sksk")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", ")
+DB_URI = os.environ.get("DB_URI", skks")
